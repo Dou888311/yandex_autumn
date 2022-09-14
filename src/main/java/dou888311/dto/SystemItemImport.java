6b80e4e4fec151b9c1bfe168b037225f25a2df9f
dou888311 <zaragatchina@gmail.com>
2022-09-14 18:29:49 +0300

    added Builder

--- src/main/java/dou888311/dto/SystemItemImport.java
+++ src/main/java/dou888311/dto/SystemItemImport.java
@@ -30,4 +30,45 @@ public class SystemItemImport {
                 .setDate(date)
                 .build();
     }
+
+    public SystemItemImport(SystemItem item) {
+        id = item.getId();
+        url = item.getUrl();
+        parentId = item.getParentId();
+        type = item.getType();
+        size = item.getSize();
+    }
+
+    public static class SystemItemImportBuilder {
+        private final SystemItemImport item = new SystemItemImport();
+
+        public SystemItemImportBuilder id(String id) {
+            item.id = id;
+            return this;
+        }
+
+        public SystemItemImportBuilder url(String url) {
+            item.url = url;
+            return this;
+        }
+
+        public SystemItemImportBuilder parentId(String parentId) {
+            item.parentId = parentId;
+            return this;
+        }
+
+        public SystemItemImportBuilder type(SystemItemType type) {
+            item.type = type;
+            return this;
+        }
+
+        public SystemItemImportBuilder size(int size) {
+            item.size = size;
+            return this;
+        }
+
+        public SystemItemImport build() {
+            return item;
+        }
+    }
 }
